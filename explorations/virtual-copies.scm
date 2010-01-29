d9f6d560c821a9918986e0d0f0079a1a603928bf
Alexey Radul <axch@mit.edu>
2010-01-29 01:59:49 +0000

    The self-loading thing is wrong and useless.

--- explorations/virtual-copies.scm
+++ explorations/virtual-copies.scm
@@ -1,29 +1,3 @@
-(define (cf-conditionally filename)
-  (fluid-let ((sf/default-syntax-table (nearest-repl/environment)))
-    (sf-conditionally filename))
-  (if (not (file-processed? filename "bin" "com"))
-      (compile-bin-file filename)))
-
-(define (load-compiled filename)
-  (cf-conditionally filename)
-  (load filename))
-
-(define (self-relatively thunk)
-  (if (current-eval-unit #f)
-      (with-working-directory-pathname
-       (directory-namestring (current-load-pathname))
-       thunk)
-      (thunk)))
-
-(define (load-relative filename)
-  (self-relatively (lambda () (load filename))))
-
-(define (load-relative-compiled filename)
-  (self-relatively (lambda () (load-compiled filename))))
-
-
-(load-relative "../propagator/art/load")
-
 ;;;; Boring data structures
 
 ;;; I want to be able to define copy sets that have systematically
