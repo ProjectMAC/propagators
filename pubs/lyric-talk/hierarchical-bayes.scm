a13abfa27fcc008ce32d4a7e1558b341914c4782
Alexey Radul <axch@mit.edu>
2010-02-02 05:18:06 +0000

    A different way to write the same code; variable elimination!

--- pubs/lyric-talk/hierarchical-bayes.scm
+++ pubs/lyric-talk/hierarchical-bayes.scm
@@ -53,3 +53,29 @@
 	(stochastic-thunk->distribution
 	 (lambda ()
 	   (experiment data))))))
+
+(define (integrate-out thunk)
+  (distribution-splice
+   (let ((thunk-distribution (stochastic-thunk->distribution thunk)))
+     (distribution/determine! thunk-distribution)
+     thunk-distribution)))
+
+(define (for-each-integrating-out proc lst)
+  (integrate-out
+   (lambda ()
+     (if (null? lst)
+	 'done
+	 (begin
+	   (for-each-integrating-out proc (cdr lst))
+	   (proc (car lst)))))))
+
+(define (experiment data)
+  (let ((bag-type (bag-prior)))
+    (for-each-integrating-out
+     (lambda (coin-data)
+       (let ((flip-this-coin (draw-coin bag-type)))
+	 (for-each (lambda (coin-datum)
+		     (observe! (equal? (flip-this-coin) coin-datum)))
+		   coin-data)))
+     data)
+    bag-type))
