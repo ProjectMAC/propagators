03c8dd9db7f7eafcf064b65b535ececf10d1505f
Alexey Radul <axch@mit.edu>
2010-02-02 14:39:10 +0000

    Wrote down a demo of Scheme; also clarified what I want to do for the rolling-dice portion.

--- pubs/lyric-talk/rolling-dice.scm
+++ pubs/lyric-talk/rolling-dice.scm
@@ -2,12 +2,18 @@
   (discrete-select (1 1/6) (2 1/6) (3 1/6)
 		   (4 1/6) (5 1/6) (6 1/6)))
 
+(define (roll-two-dice)
+  (+ (roll-die) (roll-die)))
+
+roll-two-dice
+
+(stochastic-thunk->distribution roll-two-dice)
+
+(sample (stochastic-thunk->distribution roll-two-dice))
 
 (pp
  (distribution->alist
-  (stochastic-thunk->distribution
-   (lambda ()
-     (+ (roll-die) (roll-die))))))
+  (stochastic-thunk->distribution roll-two-dice)))
 
 (pp
  (distribution->alist
---
+++ pubs/lyric-talk/scheme.scm
@@ -0,0 +1,46 @@
+5
+
+(+ 1 1)
+
+(* 4 3)
+
+(* (+ 2 3) (- 3 2))
+
++
+
+
+(define x 4)
+
+(+ x x)
+
+(define (add4 x)
+  (+ x 4))
+
+(add4 7)
+
+x
+
+
+(let ((x (* 3 4)))
+  (+ x (- x 7)))
+
++
+
+(lambda (x)
+  (+ 5 x))
+
+(define add5
+  (lambda (x)
+    (+ 5 x)))
+
+(add5 9)
+
+(define count
+  (let ((counter 0))
+    (lambda ()
+      (set! counter (+ counter 1))
+      counter)))
+
+(count)
+(count)
+(count)
