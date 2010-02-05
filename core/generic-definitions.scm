1fb352917e7960231a1ec59a2fe4d59d4f07540e
Alexey Radul <axch@mit.edu>
2010-02-05 16:26:23 +0000

    I understand constraints well enough to integrate them into the base system, part 1: Moved the definitions of the constraints as "macros" to the macros section.

--- core/generic-definitions.scm
+++ core/generic-definitions.scm
@@ -137,3 +137,33 @@
     (switch control input if-true)
     (switch not-control input if-false)))
 
+(define (sum-constraint a1 a2 sum)
+  (adder a1 a2 sum)
+  (subtractor sum a1 a2)
+  (subtractor sum a2 a1))
+
+(define (product-constraint m1 m2 product)
+  (multiplier m1 m2 product)
+  (divider product m1 m2)
+  (divider product m2 m1))
+
+(define (quadratic-constraint x x^2)
+  (squarer x x^2)
+  (sqrter x^2 x))
+
+(define (not-constraint p1 p2)
+  (inverter p1 p2)
+  (inverter p2 p1))
+
+(define (identity-constraint c1 c2)
+  (pass-through c1 c2)
+  (pass-through c2 c1))
+
+(define (and-constraint p1 p2 conjunction)
+  (conjoiner p1 p2 conjunction)
+  (rconjoiner conjunction p1 p2))
+
+(define (or-constraint p1 p2 disjunction)
+  (disjoiner p1 p2 disjunction)
+  (rdisjoiner disjunction p1 p2))
+
