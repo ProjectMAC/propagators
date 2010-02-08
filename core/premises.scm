f2f52a5fe0d6fc110c4029c9c7ed5895c2df6b03
Alexey Radul <axch@mit.edu>
2010-02-08 23:14:15 +0000

    These two are really about the premises.

--- core/premises.scm
+++ core/premises.scm
@@ -57,3 +57,17 @@
     (lambda ()
       (initialize-scheduler)
       (reset-premise-info!))))
+
+(define (disbelieving-func premise thunk)
+  (let ((old-belief (premise-in? premise)))
+    (kick-out! premise)
+    (let ((answer (thunk)))
+      (if old-belief
+	  (bring-in! premise)
+	  (kick-out! premise))
+      answer)))
+
+(define-syntax disbelieving
+  (syntax-rules ()
+    ((_ premise body ...)
+     (disbelieving-func premise (lambda () body ...)))))
--- core/test/test-utils.scm
+++ core/test/test-utils.scm
@@ -1,17 +1,3 @@
-(define (disbelieving-func premise thunk)
-  (let ((old-belief (premise-in? premise)))
-    (kick-out! premise)
-    (let ((answer (thunk)))
-      (if old-belief
-	  (bring-in! premise)
-	  (kick-out! premise))
-      answer)))
-
-(define-syntax disbelieving
-  (syntax-rules ()
-    ((_ premise body ...)
-     (disbelieving-func premise (lambda () body ...)))))
-
 (define (fail-all cells)
   (process-one-contradiction
    (apply append (map v&s-support (filter v&s? (map tms-query (filter tms? (map content cells))))))))
