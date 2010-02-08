dec341992845fc3be8399121bced6dc98fb091bc
Alexey Radul <axch@mit.edu>
2010-02-08 22:51:01 +0000

    Those test utilities really should live in their own place.

--- core/test/load.scm
+++ core/test/load.scm
@@ -8,22 +8,9 @@
 (define (load-relative filename)
   (self-relatively (lambda () (load filename))))
 
-(define (disbelieving-func premise thunk)
-  (let ((old-belief (premise-in? premise)))
-    (kick-out! premise)
-    (let ((answer (thunk)))
-      (if old-belief
-	  (bring-in! premise)
-	  (kick-out! premise))
-      answer)))
-
-(define-syntax disbelieving
-  (syntax-rules ()
-    ((_ premise body ...)
-     (disbelieving-func premise (lambda () body ...)))))
-
 (for-each load-relative
-  '("core-test"
+  '("test-utils"
+    "core-test"
     "dependencies-test"
     "profiler-test"
     "partial-compounds-test"
