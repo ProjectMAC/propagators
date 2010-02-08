be653f91709bba016b82cae05d3c441ce7577c1e
Alexey Radul <axch@mit.edu>
2010-02-08 23:22:07 +0000

    These are utils.  At least they are now out of the load file.

--- extensions/test/test-utils.scm
+++ extensions/test/test-utils.scm
@@ -13,3 +13,33 @@
 
 (define map-consistent-states (walker->mapper for-each-consistent-state))
 
+(define-method generic-match ((pattern <vector>) (object :symb-ineq))
+  (generic-match
+   pattern (vector 'symb-ineq (symb-ineq-expression object)
+		   (symb-ineq-local object)
+		   (symb-ineq-global object))))
+
+(define-method generic-match ((pattern <vector>) (object :symbolic))
+  (generic-match
+   pattern (vector 'symbolic (symbolic-expression object)
+		   (symbolic-metadata object))))
+
+(define-method generic-match ((pattern <vector>) (object :symbolic-metadata))
+  (generic-match
+   pattern (vector 'metadata (symbolic-variable-order object)
+		   (symbolic-substitutions object)
+		   (symbolic-residual-equations object))))
+
+(define-method generic-match ((pattern <vector>) (object rtd:frs))
+  (if (stale-frs? object)
+      (generic-match
+       pattern (vector 'stale-frs (frs-value object)
+		       (frs-support object)))
+      (generic-match
+       pattern (vector 'frs (frs-value object)
+		       (frs-support object)))))
+
+(define-method generic-match ((pattern <vector>) (object rtd:frpremise))
+  (generic-match
+   pattern (vector 'frp (frpremise-identity object)
+		   (frpremise-timestamp object))))
