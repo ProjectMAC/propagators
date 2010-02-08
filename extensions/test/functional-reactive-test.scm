d522358cb79aea9774fc8040f6724019a493dbcf
Alexey Radul <axch@mit.edu>
2010-02-08 02:20:22 +0000

    Assert-equal becomes produces.

--- extensions/test/functional-reactive-test.scm
+++ extensions/test/functional-reactive-test.scm
@@ -5,44 +5,40 @@
    (generic-match #(frp seconds 0) (make-frpremise 'seconds 0)))
 
  (define-test (glitch)
-   (initialize-scheduler)
-   (define-cell one)
-   (define-cell seconds)
-   (define-cell seconds+one)
-   (adder one seconds seconds+one)
-   (add-content one 1)
-   (add-content seconds (make-frs 0 (make-frpremise 'seconds 0)))
-   (run)
-   (assert-matches
-    #(frs 1 (#(frp seconds 0)))
-    (content seconds+one))
+   (interaction
+    (initialize-scheduler)
+    (define-cell one)
+    (define-cell seconds)
+    (define-cell seconds+one)
+    (adder one seconds seconds+one)
+    (add-content one 1)
+    (add-content seconds (make-frs 0 (make-frpremise 'seconds 0)))
+    (run)
+    (content seconds+one)
+    (produces #(frs 1 (#(frp seconds 0))))
 
-   (define-cell seconds+one-again)
-   (define-cell glitchable)
-   (<? seconds seconds+one-again glitchable)
-   (add-content seconds+one-again (content seconds+one))
-   (run)
-   (assert-matches
-    #(frs #t (#(frp seconds 0)))
-    (content glitchable))
+    (define-cell seconds+one-again)
+    (define-cell glitchable)
+    (<? seconds seconds+one-again glitchable)
+    (add-content seconds+one-again (content seconds+one))
+    (run)
+    (content glitchable)
+    (produces #(frs #t (#(frp seconds 0))))
    
-   (add-content seconds (make-frs 1 (make-frpremise 'seconds 1)))
-   (assert-matches #(frs 1 (#(frp seconds 1))) (content seconds))
-   (run)
-   (assert-matches
-    #(frs 2 (#(frp seconds 1)))
-    (content seconds+one))
-   ;; Rather than glitching, it should notice that its input is out of
-   ;; date
-   (assert-matches
-    #(frs #t (#(frp seconds 0)))
-    (content glitchable))
+    (add-content seconds (make-frs 1 (make-frpremise 'seconds 1)))
+    (content seconds)
+    (produces #(frs 1 (#(frp seconds 1))))
+    (run)
+    (content seconds+one)
+    (produces #(frs 2 (#(frp seconds 1))))
+    ;; Rather than glitching, it should notice that its input is out of
+    ;; date
+    (content glitchable)
+    (produces #(frs #t (#(frp seconds 0))))
 
-   ;; But when updated, it should propagate
-   (add-content seconds+one-again (content seconds+one))
-   (run)
-   (assert-matches
-    #(frs #t (#(frp seconds 1)))
-    (content glitchable))
-   
-   ))
+    ;; But when updated, it should propagate
+    (add-content seconds+one-again (content seconds+one))
+    (run)
+    (content glitchable)
+    (produces #(frs #t (#(frp seconds 1))))))
+ )
