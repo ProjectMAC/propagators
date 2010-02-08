2012b711b2484253ced3ce5b025c7028dba32db1
Alexey Radul <axch@mit.edu>
2010-02-08 02:17:44 +0000

    Chewing up assert-equals into produces.

--- extensions/test/symbolics-ineq-test.scm
+++ extensions/test/symbolics-ineq-test.scm
@@ -245,95 +245,108 @@
    )
 
  (define-test (ineq-enforcer-smoke)
-   (initialize-scheduler)
-   (define-cell five)
-   ((constant (make-tms (supported 5 '(joe)))) five)
-   (define-cell victim)
-   ((ineq-enforcer '<) five victim)
-   ((ineq-enforcer '<) victim five)
-   ;; Doesn't detect the contradiction without a plunk
-   (assert-equal 'done (run))
+   (interaction
+    (initialize-scheduler)
+    (define-cell five)
+    ((constant (make-tms (supported 5 '(joe)))) five)
+    (define-cell victim)
+    ((ineq-enforcer '<) five victim)
+    ((ineq-enforcer '<) victim five)
+    ;; Doesn't detect the contradiction without a plunk
+    (run)
+    (produces 'done)
 
-   (plunker victim)
-   (assert-equal '(contradiction (joe)) (run)))
+    (plunker victim)
+    (run)
+    (produces '(contradiction (joe)))))
 
  (define-test (ineq-constraint-smoke)
-   (initialize-scheduler)
-   (define-cell four)
-   ((constant 4) four)
-   (define-cell ctl)
-   (define-cell victim)
-   (>-constraint ctl four victim)
-   ((constant #t) ctl)
-   (run)
-   (assert-matches
-    (vector 'symb-ineq nothing '((< me 4 (me))) '())
-    (content victim)))
+   (interaction
+    (initialize-scheduler)
+    (define-cell four)
+    ((constant 4) four)
+    (define-cell ctl)
+    (define-cell victim)
+    (>-constraint ctl four victim)
+    ((constant #t) ctl)
+    (run)
+    (content victim)
+    (produces (vector 'symb-ineq nothing '((< me 4 (me))) '()))))
 
  (define-test (more-ineq-constraint-smoke)
-   (initialize-scheduler)
-   (define-cell three)
-   ((constant (make-tms (supported 3 '(joe)))) three)
-   (define-cell victim)
-   (define-cell ctl)
-   (<-constraint ctl three victim)
-   (>=-constraint ctl three victim)
-   ;; Doesn't detect the contradiction without a boolean plunk on the
-   ;; control
-   (assert-equal 'done (run))
+   (interaction
+    (initialize-scheduler)
+    (define-cell three)
+    ((constant (make-tms (supported 3 '(joe)))) three)
+    (define-cell victim)
+    (define-cell ctl)
+    (<-constraint ctl three victim)
+    (>=-constraint ctl three victim)
+    ;; Doesn't detect the contradiction without a boolean plunk on the
+    ;; control
+    (run)
+    (produces 'done)
 
-   (binary-amb ctl)
-   (assert-equal '(contradiction (joe)) (run)))
+    (binary-amb ctl)
+    (run)
+    (produces '(contradiction (joe)))))
 
  (define-test (even-more-ineq-constraint-smoke)
-   (initialize-scheduler)
-   (define-cell three)
-   ((constant (make-tms (supported 3 '(joe)))) three)
-   (define-cell victim1)
-   (define-cell victim2)
-   (define-cell ctl)
-   (<-constraint ctl three victim1)
-   (>=-constraint ctl three victim2)
-   (define-cell zero)
-   ((constant 0) zero)
-   (sum-constraint zero victim1 victim2)
-   ;; Doesn't detect the contradiction without a boolean plunk on the
-   ;; control
-   (assert-equal 'done (run))
+   (interaction
+    (initialize-scheduler)
+    (define-cell three)
+    ((constant (make-tms (supported 3 '(joe)))) three)
+    (define-cell victim1)
+    (define-cell victim2)
+    (define-cell ctl)
+    (<-constraint ctl three victim1)
+    (>=-constraint ctl three victim2)
+    (define-cell zero)
+    ((constant 0) zero)
+    (sum-constraint zero victim1 victim2)
+    ;; Doesn't detect the contradiction without a boolean plunk on the
+    ;; control
+    (run)
+    (produces 'done)
 
-   (binary-amb ctl)
-   ;; Doesn't detect the contradiction without a variable plunk, either
-   (assert-equal 'done (run))
+    (binary-amb ctl)
+    ;; Doesn't detect the contradiction without a variable plunk, either
+    (run)
+    (produces 'done)
 
-   (plunker victim1)
-
-   (assert-equal '(contradiction (joe)) (run)))
+    (plunker victim1)
+    (run)
+    (produces '(contradiction (joe)))))
 
  (define-test (even-more-more-ineq-constraint-smoke)
-   (initialize-scheduler)
-   (define-cell three)
-   ((constant (make-tms (supported 3 '(joe)))) three)
-   (define-cell victim1)
-   (define-cell victim2)
-   (define-cell ctl)
-   (<-constraint ctl three victim1)
-   (>=-constraint ctl three victim2)
-   (define-cell zero)
-   ((constant 0) zero)
-   (sum-constraint zero victim1 victim2)
-   ;; Doesn't detect the contradiction without a boolean plunk on the
-   ;; control.
-   (assert-equal 'done (run))
+   (interaction
+    (initialize-scheduler)
+    (define-cell three)
+    ((constant (make-tms (supported 3 '(joe)))) three)
+    (define-cell victim1)
+    (define-cell victim2)
+    (define-cell ctl)
+    (<-constraint ctl three victim1)
+    (>=-constraint ctl three victim2)
+    (define-cell zero)
+    ((constant 0) zero)
+    (sum-constraint zero victim1 victim2)
+    ;; Doesn't detect the contradiction without a boolean plunk on the
+    ;; control.
+    (run)
+    (produces 'done)
 
-   (binary-amb ctl)
-   ;; Doesn't detect the contradiction without a variable plunk, either 
-   (assert-equal 'done (run))
+    (binary-amb ctl)
+    ;; Doesn't detect the contradiction without a variable plunk, either 
+    (run)
+    (produces 'done)
 
-   (define-cell four)
-   ((constant 4) four)
-   (define-cell x)
-   (plunker x)
-   (multiplier four x victim1)
+    (define-cell four)
+    ((constant 4) four)
+    (define-cell x)
+    (plunker x)
+    (multiplier four x victim1)
 
-   (assert-equal '(contradiction (joe)) (run)))
+    (run)
+    (produces '(contradiction (joe)))))
 )
