e14113b58d83cb6585d9bba9ea44440b4c555444
Alexey Radul <axch@mit.edu>
2010-02-08 22:55:03 +0000

    These are utilities.  They should not live there.

--- core/test/test-utils.scm
+++ core/test/test-utils.scm
@@ -12,3 +12,18 @@
     ((_ premise body ...)
      (disbelieving-func premise (lambda () body ...)))))
 
+(define (fail-all cells)
+  (process-one-contradiction
+   (apply append (map v&s-support (filter v&s? (map tms-query (filter tms? (map content cells))))))))
+
+(define (for-each-consistent-state proc cells)
+  (set! cells (listify cells))
+  (let loop ((last-run-result (run)))
+    (if (eq? 'done last-run-result)
+	(begin
+	  (proc)
+	  (fail-all cells)
+	  (loop (run))))))
+
+(define map-consistent-states (walker->mapper for-each-consistent-state))
+
--- extensions/test/load.scm
+++ extensions/test/load.scm
@@ -39,21 +39,7 @@
    pattern (vector 'frp (frpremise-identity object)
 		   (frpremise-timestamp object))))
 
-(define (fail-all cells)
-  (process-one-contradiction
-   (apply append (map v&s-support (filter v&s? (map tms-query (filter tms? (map content cells))))))))
-
-(define (for-each-consistent-state proc cells)
-  (set! cells (listify cells))
-  (let loop ((last-run-result (run)))
-    (if (eq? 'done last-run-result)
-	(begin
-	  (proc)
-	  (fail-all cells)
-	  (loop (run))))))
-
-(define map-consistent-states (walker->mapper for-each-consistent-state))
-
+(load-relative "../../core/test/test-utils")
 (load-relative "inequality-test")
 (load-relative "symbolics-test")
 (load-relative "symbolics-ineq-test")
