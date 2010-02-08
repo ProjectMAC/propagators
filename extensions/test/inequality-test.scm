3622b35bfc008b350a9a5f4d5e6d3f4a3c4b7e08
Alexey Radul <axch@mit.edu>
2010-02-08 02:07:40 +0000

    inequality-test benefits from the check macro considerably, as far as number of hyphen-separated subtokens is concerned.

--- extensions/test/inequality-test.scm
+++ extensions/test/inequality-test.scm
@@ -4,64 +4,64 @@
 (in-test-group
  inequalities
 
- (define-each-test
-   (assert-true (tautological-ineq? (make-tautological-ineq)))
-   (assert-true (contradictory-ineq? (make-contradictory-ineq)))
+ (define-each-check
+   (tautological-ineq? (make-tautological-ineq))
+   (contradictory-ineq? (make-contradictory-ineq))
 
-   (assert-true (tautological-ineq?
-		 (test-try-ineq (make-inequality '> '(expt x 2)))))
-   (assert-true (contradictory-ineq?
-		 (test-try-ineq (make-inequality '< '(expt x 2)))))
+   (tautological-ineq?
+    (test-try-ineq (make-inequality '> '(expt x 2))))
+   (contradictory-ineq?
+    (test-try-ineq (make-inequality '< '(expt x 2))))
 
-   (assert-equal
+   (equal?
     (make-solved-inequality '< 'y -3)
     (test-try-ineq (make-inequality '< '(+ y 3))))
-   (assert-equal
+   (equal?
     (make-solved-inequality '< 'y -3/2)
     (test-try-ineq (make-inequality '< '(+ (* 2 y) 3))))
-   (assert-equal
+   (equal?
     (make-solved-inequality '<= 'x 0)
     (test-try-ineq (make-inequality '<= '(* 23 x))))
-   (assert-equal
+   (equal?
     (make-solved-inequality '>= 'x 0)
     (test-try-ineq (make-inequality '<= '(* -23 x))))
-   (assert-equal
+   (equal?
     (make-solved-inequality '< 'x 4/23)
     (test-try-ineq (make-inequality '> '(+ 4 (* -23 x)))))
 
-   (assert-equal
+   (equal?
     'failed
     (test-try-ineq (make-inequality '> '(+ x y))))
-   (assert-equal
+   (equal?
     'failed
     (test-try-ineq (make-inequality '> '(+ 5 (exp y) 8))))
-   (assert-equal
+   (equal?
     'failed
     (test-try-ineq (make-inequality '> '(+ (expt z 2) z 3))))
    
-   (assert-equal '() (simplify-inequalities '()))
-   (assert-equal
+   (equal? '() (simplify-inequalities '()))
+   (equal?
     (list (make-inequality '> 'x))
     (simplify-inequalities
      (list (make-inequality '> 'x))))
-   (assert-equal
+   (equal?
     (list (make-solved-inequality '< 'x 5/2))
     (simplify-inequalities
      (list (make-inequality '< '(- x 4))
 	   (make-inequality '< '(- (* 2 x) 5)))))
-   (assert-equal
+   (equal?
     (list (make-solved-inequality '< 'y 4)
 	  (make-solved-inequality '< 'x 5/2))
     (simplify-inequalities
      (list (make-inequality '< '(- y 4))
 	   (make-inequality '< '(- (* 2 x) 5)))))
 
-   (assert-equal
+   (equal?
     (list (make-solved-inequality '< 'x 4))
     (simplify-inequalities
      (list (make-inequality '< '(- x 4))
 	   (make-inequality '<= '(- (* 2 x) 8)))))
-   (assert-equal
+   (equal?
     (list (make-solved-inequality '< 'x 4)
 	  (make-inequality '>= '(+ (expt z 2) z 2)))
     (simplify-inequalities
@@ -69,34 +69,34 @@
 	   (make-inequality '>= '(+ z 2 (* z z)))
 	   (make-inequality '<= '(- (* 2 x) 8)))))
 
-   (assert-equal
+   (equal?
     (list (make-solved-inequality '>= 'y 4)
 	  (make-solved-inequality '< 'x 5/2))
     (simplify-inequalities
      (list (make-inequality '>= '(- y 4))
 	   (make-inequality '< '(- (* 2 x) 5)))))
-   (assert-equal
+   (equal?
     #f
     (simplify-inequalities
      (list (make-inequality '>= '(- x 4))
 	   (make-inequality '< '(- (* 2 x) 5)))))
-   (assert-equal
+   (equal?
     #f
     (simplify-inequalities
      (list (make-inequality '>= '(- x 4))
 	   (make-inequality '>= '(+ z 2 (* z z)))
 	   (make-inequality '< '(- (* 2 x) 5)))))
-   (assert-equal
+   (equal?
     '()
     (simplify-inequalities
      (list (make-inequality '>= '(+ (* 3 x) (* -2 x) (* -1 x) 4)))))
-   (assert-equal
+   (equal?
     #f
     (simplify-inequalities
      (list (make-inequality '>= '(+ (* 3 x) (* -2 x) (* -1 x) 4))
 	   (make-inequality '>= '(+ (* 3 y) (* -2 y) (* -1 y) -4)))))
    
-   (assert-equal
+   (equal?
     (make-inequality '< -1)
     (transitive-ineq (make-solved-inequality '> 'x 4)
 		     (make-solved-inequality '< 'x 5)))
