8de38343ae2b5bcc9c10442362b7962118b28dba
Alexey Radul <axch@mit.edu>
2010-02-09 21:58:36 +0000

    Copyrights and GPLv3.

--- extensions/prop-dot.scm
+++ extensions/prop-dot.scm
@@ -1,7 +1,23 @@
-;;; Code to render propagator networks as dot graphs.
+;;; ----------------------------------------------------------------------
+;;; Copyright 2009-2010 Taylor Campbell and Alexey Radul.
+;;; ----------------------------------------------------------------------
+;;; This file is part of Propagator Network Prototype.
+;;; 
+;;; Propagator Network Prototype is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;; 
+;;; Propagator Network Prototype is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;; 
+;;; You should have received a copy of the GNU General Public License
+;;; along with Propagator Network Prototype.  If not, see <http://www.gnu.org/licenses/>.
+;;; ----------------------------------------------------------------------
 
-;;; Collected, with permission, from Taylor Campbell.
-;;; Massaged by axch to work on axch's propagators.
+;;; Code to render propagator networks as dot graphs.
 
 ;;; TODO Further improvements
 
