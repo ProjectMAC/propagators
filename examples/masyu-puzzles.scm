7fede6f9c3823992ef48d70759dd9824cee02c51
Alexey Radul <axch@mit.edu>
2010-02-09 22:03:03 +0000

    I don't need a separate file for keeping the puzzle definitions.

--- examples/masyu-puzzles.scm
+++ examples/masyu-puzzles.scm
@@ -21,3 +21,26 @@
 "            X  O O"
 "X O O    X      OO"
 "       O    XX    "))
+
+#;
+(do-puzzle '(
+"           OX O  OX    X  O    X    "
+"X   O X        X     X        O   X "
+"   X     X  O               X O     "
+"   X         X   X X    XX          "
+"  O    O  XX    X    O    O XX  O  X"
+" O   X       O         O        O  O"
+"          O    X  O     X X    O  O "
+"X   X   X   O O      X  X   X       "
+"        X  O       O XO   O     X X "
+"X  O   O      X O       O      O    "
+"    X    O       O   X        X X X "
+" X O       X  O X  O    OO     O    "
+"      X X O  XO X      X   OX O O   "
+" O X       O         X           X  "
+"  O   XX  X     OX X      X  OO  X  "
+"           O X        XX   O   X    "
+"        O         O  O     O       X"
+"X   O X        X       X        OO  "
+" O X  OO  X     O  O     O   X      "
+"             X      X     X     O O "))
