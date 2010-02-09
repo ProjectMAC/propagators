dccef6536a679882593108b3e177b8f8917ac37b
Alexey Radul <axch@mit.edu>
2010-02-09 05:31:42 +0000

    Moved the profiler tests out of core/test/ and into support/test/. Also sweeping the README for truth level.

--- support/test/load.scm
+++ support/test/load.scm
@@ -9,10 +9,4 @@
   (self-relatively (lambda () (load filename))))
 
 (for-each load-relative
-  '("core-test"
-    "dependencies-test"
-    "partial-compounds-test"
-    "switches-test"
-    "abstraction-test"
-    "compound-merges-test"
-    "barometer-test"))
+  '("profiler-test"))
