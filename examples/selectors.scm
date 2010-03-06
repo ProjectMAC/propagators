2e01f8401c2c8582fd780b9565753ce18212520c
Alexey Radul <axch@mit.edu>
2010-03-06 19:30:36 +0000

    Renaming some files.

--- examples/selectors.scm
+++ examples/selectors.scm
@@ -1,50 +1,159 @@
-(define (plan-trip source destination method)
-  (let-cells (cost time annoyance)
-    (air-travel      source destination method cost time annoyance)
-    (train-travel    source destination method cost time annoyance)
-    (intercity-bus-travel source destination method cost time annoyance)
-    (car-travel      source destination method cost time annoyance)
-    (subway-travel   source destination method cost time annoyance)
-    (city-bus-travel source destination method cost time annoyance)
-    (taxi-travel     source destination method cost time annoyance)
-    (bicycle-travel  source destination method cost time annoyance)
-    (walking-travel  source destination method cost time annoyance)))
+(define (plan-trip go? segment pain answer)
+  (let-cells (air-go? air-input air-pain air-answer
+	      train-go? train-input train-pain train-answer
+	      bus-go? bus-input bus-pain bus-answer
+	      method)
+    (plan-air air-go? air-input air-pain air-answer)
+    (plan-train train-go? train-input train-pain train-answer)
+    (plan-bus bus-go? bus-input bus-pain bus-answer)
+    (critic go? air-pain train-pain bus-pain method)
+    (selector go? method air-go? train-go? bus-go?)
+    (answerer go? method air-answer train-answer bus-answer answer)
+    ))
 
-(define (doit go-worldview input output build-it)
-  ;; The input is a bunch of conditional input information.  The
-  ;; output is a bunch of conditional output information.  The
-  ;; go-worldview starts empty.  When it is written, the device reads
-  ;; that worldview from the input, and goes off and does the
-  ;; computation.  When an answer shows up on the internal output, it
-  ;; is reconditioned on the go-worldview and written to the external
-  ;; output.  Also at this point, the device is reset for the next use
-  ;; by rebuilding the internal input and output cells, and clearing
-  ;; the go-worldview cell.  The outside world knows the device is
-  ;; ready for use by examining the go-worldview cell.  Substructure
-  ;; assumes responsibility for not jumping the gun, and writing the
-  ;; full answer to the internal output cell all at once.
-  ;; The build-it procedure attaches the internal device to the
-  ;; internal input and output cells.
-  (let-cell status
-    (define (builder status)
-      (define (do-build)
-	(if (nothing? (content status))
-	    (let-cells (in-worldview in-input in-output)
-	      (build-it in-input in-output)
-	      (worldview-selector go-worldview input in-input)
-	      ;; This indirection prevents worldview-attacher from
-	      ;; rewriting the status cell when the go-worldview
-	      ;; changes (assuming the in-worldview cell rejects
-	      ;; updates).
-	      (copy go-worldview in-worldview)
-	      (worldview-attacher in-worldview in-output output status)
-	      (add-content status 'built))))
-      (propagator status do-build))
-    (define (destroyer status)
-      (define (do-destroy)
-	(if (eq? 'done (content status))
-	    (begin (add-content status *clear*)
-		   (add-content go-worldview *clear*))))
-      (propagator status do-destroy))
-    (builder status)
-    (destroyer status)))
+(define (choice-node . alternatives)
+  (lambda (go? segment)
+    (let-cell method
+      (let ((opt-cells
+	     (map (lambda (alternative)
+		    (let-cells (opt-go? opt-segment)
+		      (alternative opt-go? opt-segment)
+		      (list opt-go? opt-segment)))
+		  alternatives)))
+	(apply critic go? method (map cadr opt-cells))
+	(apply push-selector go? go?     method (map car opt-cells))
+	(apply push-selector go? segment method (map cadr opt-cells))
+	(apply pull-selector go? segment method (map cadr opt-cells))))))
+#;
+(define plan-trip
+  (choice-node plan-air plan-train plan-subway plan-walk))
+
+(define (critic go? method . answers)
+  ;; Choose the method that promises least pain
+  ...)
+
+(define (push-selector go? pushee method . targets)
+  ;; Conditionally shove the contents of the pushee into the target
+  ;; indicated by the method
+  ...)
+
+(define (pull-selector go? pullee method . targets)
+  ;; Conditionally shove the contents of the target indicated by the
+  ;; method into the pullee (and tag it with the identity of the
+  ;; method?)
+  ...)
+
+(define (plan-air go? segment pain answer)
+  (fast-air-estimate segment pain answer)
+  (let-cells (to-air-input by-air-input from-air-input
+	      to-air-pain by-air-pain from-air-pain
+	      to-air-answer by-air-answer from-air-answer
+	      to-air-go? by-air-go? from-air-go?)
+    (forwarder go? to-air-go? by-air-go? from-air-go?)
+    (summer go? to-air-pain by-air-pain from-air-pain pain)
+    (compounder go? to-air-answer by-air-answer from-air-answer answer)
+
+    (select-source-airport go? segment to-air-input by-air-input)
+    (plan-trip to-air-go? to-air-input to-air-pain to-air-answer)
+
+    (between-airports by-air-go? by-air-input by-air-pain by-air-answer)
+
+    (select-dest-airport go? segment from-air-input by-air-input)
+    (plan-trip from-air-go? from-air-input from-air-pain from-air-answer)
+    ))
+
+(define (split-node estimator segment-splitter . stages)
+  (lambda (go? segment)
+    (estimator segment)
+    (let ((stage-cells
+	   (map (lambda (stage)
+		  (let-cells (stage-go? stage-segment)
+		    (forwarder go? stage-go?)
+		    (stage stage-go? stage-segment)
+		    (list stage-go? stage-segment)))
+		stages)))
+      (apply answer-compounder go? segment (map cadr stage-cells))
+      (apply segment-splitter go? segment (map cadr stage-cells)))))
+#|
+(define plan-air
+  (split-node fast-air-estimate airport-splitter
+	      plan-trip between-airports plan-trip))
+
+(define plan-train
+  (split-node fast-train-estimate station-splitter
+	      plan-trip between-stations plan-trip))
+
+(define plan-subway
+  (split-node fast-subway-estimate stop-splitter
+	      plan-trip between-stops plan-trip))
+|#
+(define (answer-compounder go? out . subanswers)
+  ...)
+
+(define (forwarder go? subgo?)
+  ;; If the "go" signal is suitably "deep-go", forward it.
+  ...)
+
+;;; The actual specific planners
+(define (plan-walk go? segment)
+  ((constant (make-trip-segment-key 'method 'just-walk)) segment)
+  (time-est segment (& 3 (/ mile hour)) segment)
+  ;; Or more for food, etc if it takes a long time
+  ((constant (make-trip-segment-key 'cost (& 0 dollar))) segment)
+  ;; Or: Some fixed function of time
+  ((constant (make-trip-segment-key 'pain (& 0 crap))) segment))
+
+(define (fast-air-estimate segment)
+  ((constant (make-trip-segment-key 'method 'fly)) segment)
+  ((constant (make-trip-segment-key 'time (& 1 day))) segment)
+  ((constant (make-trip-segment-key 'cost (& 500 dollar))) segment)
+  ((constant (make-trip-segment-key 'pain (& 200 crap))) segment))
+
+(define (airport-splitter go? segment to by from)
+  ; (start segment) -> (start to)
+  ; (end segment) -> (end from)
+  ; (the-airport (start segment)) -> (end to) (start by)
+  ; (the-airport (end segment)) -> (end by) (start from)
+  ...) ;; Ditto stations, stops
+
+(define (between-airports go? segment)
+  ; Complicated task-specific stuff...
+  ...)
+
+(define (fast-train-estimate segment)
+  ((constant (make-trip-segment-key 'method 'take-the-train)) segment)
+  ;; Plus two hours for two-from the station?
+  (time-est segment (& 50 (/ mile hour)) segment)
+  ((constant (make-trip-segment-key 'cost (& 50 dollar))) segment)
+  ((constant (make-trip-segment-key 'pain (& 25 crap))) segment))
+
+(define (between-stations go? segment)
+  ; Complicated task-specific stuff...
+  ...)
+
+(define (fast-subway-estimate segment)
+  (let-cells (same-city? same-city-answer)
+    (switch same-city? same-city-answer segment)
+    ((constant (make-trip-segment-key 'method 'subway)) same-city-answer)
+    ((constant (make-trip-segment-key 'time (& 1 hour)) same-city-answer))
+    ((constant (make-trip-segment-key 'cost (& 2 dollar))) same-city-answer)
+    ((constant (make-trip-segment-key 'pain (& 25 crap))) same-city-answer)
+    (same-city segment same-city?)))
+
+(define (between-stops go? segment)
+  ; Complicated task-specific stuff...
+  ...)
+
+#|
+ (initialize-scheduler)
+ (define-cell foo)
+ (add-content foo (make-trip-segment-key 'start 'home 'end 'met))
+ (fast-air-estimate foo)
+ (run)
+|#
+;;; TODO How does one watch this search and adjust the fast estimates
+;;; in light of backtracks caused by later refinements?
+
+;;; Unit system hacking
+(define dollar ampere)
+(define crap ampere)
