254129454bfecb38a73d9961ed3e7380d4109ba6
Gerald Jay Sussman <gjs@mit.edu>
2010-04-22 22:32:14 +0000

    More random notations for the same thing.  And a little indentation to make Emacs happy with the block comment.

--- explorations/ce-amp.scm
+++ explorations/ce-amp.scm
@@ -81,6 +81,24 @@
 	(voltage-divider-slice Rb1 Rb2)
 	))))
 
+(define (ce-amplifier +rail -rail sigin sigout)
+  (let-nodes (en bn cn)
+    (let-cells (input-impedance output-impedance maximum-output-swing gain)
+      (named
+       (Rb1  (resistor  +rail bn))
+       (Rb2  (resistor  bn -rail))
+       (Rc   (resistor  +rail cn))
+       (Re   (resistor  en -rail))
+       (Cin  (capacitor sigin bn))
+       (Cout (capacitor cn sigout))
+       (Q    (NPN-bjt en bn cn)))
+      (= gain (/ (the resistance Rc) (the resistance Re)))
+      (= input-impedance (parallel (the resistance Rb1)
+				   (the resistance Rb2)))
+      (= output-impedance ...)
+      (voltage-divider-slice Rb1 Rb2)
+      )))
+
 (define (breadboard)
   (circuit (+V gnd in out) ()
     (parts
@@ -91,31 +109,64 @@
      )))
 
 #|
-(define-circuit test (breadboard))
+ (define-circuit test (breadboard))
 
-(add-content (the resistance Rb1 amp test) (& 51000 ohm))
-(add-content (the resistance Rb2 amp test) (& 10000 ohm))
-(add-content (the resistance Rb1 amp test) (& 1000 ohm))
-(add-content (the capacitance Cin amp test) (& 10e-6 farad))
-(add-content (the capacitance Cout amp test) (& 10e-6 farad))
+ (add-content (the resistance Rb1 amp test) (& 51000 ohm))
+ (add-content (the resistance Rb2 amp test) (& 10000 ohm))
+ (add-content (the resistance Rb1 amp test) (& 1000 ohm))
+ (add-content (the capacitance Cin amp test) (& 10e-6 farad))
+ (add-content (the capacitance Cout amp test) (& 10e-6 farad))
 
-(add-content (the input-impedance amp test)
-	     (lambda (z)
-	       (> (magnitude z) (& 5000 ohm))))
+ (add-content (the input-impedance amp test)
+	      (lambda (z)
+		(> (magnitude z) (& 5000 ohm))))
 
-(add-content (the gain amp test) 5)
+ (add-content (the gain amp test) 5)
 
 
-(add-content (the strength VCC test) (& 15 volt))
-(add-content (the incremental strength vin test) (& 0.1 volt))
+ (add-content (the strength VCC test) (& 15 volt))
+ (add-content (the incremental strength vin test) (& 0.1 volt))
 
-(run)
+ (run)
 
-(content (the resistance Rc amp test))
-;;; Should be about (& 5000 ohm)
+ (content (the resistance Rc amp test))
+ ;;; Should be about (& 5000 ohm)
 
-(content (the incremental voltage vout test))
-(content (the power Q amp test))
-
+ (content (the incremental voltage vout test))
+ (content (the power Q amp test))
 |#
 
+;;; Yet another theory.
+(define (ce-amplifier)
+  (let ((Rb1 (resistor))
+	(Rb2 (resistor))
+	(Rc  (resistor))
+	(Re  (resistor))
+	(Cin (capacitor))
+	(Cout (capacitor))
+	(Q (NPN-bjt)))
+    (let ((+rail (node (the top Rb1) (the top Rc)))
+	  (-rail (node (the bottom Rb2) (the bottom Re)))
+	  (en (node (the top Re) (the emitter Q)))
+	  (cn (node (the bottom Rc) (the top Cout) (the collector Q)))
+	  (bn (node (the bottom Rb1) (the top Rb2) (the base Q)
+		    (the bottom Cin)))
+	  (sigin (the top Cin))
+	  (sigout (the bottom Cout)))
+      (let ((gain (/ (the resistance Rc) (the resistance Re)))
+	    (input-impedance (parallel (the resistance Rb1)
+				       (the resistance Rb2)))
+	    (output-impedance ...)
+	    (power (sum (map (the power) (list Rb1 Rb2 Rc Re Cin Cout Q)))))
+	(voltage-divider-slice Rb1 Rb2)))))
+
+(define (breadboard)
+  (let ((VCC (bias-voltage-source))
+	(vin (signal-voltage-source))
+	(vout (signal-voltage-probe))
+	(amp (ce-amplifier)))
+    (let ((gnd (node (the bottom VCC) (the bottom vin) (the bottom vout)
+		     (the -rail amp)))
+	  (+V (node (the top VCC) (the +rail amp)))
+	  (in (node (the top vin) (the sigin amp)))
+	  (out (node (the top vout) (the sigout amp)))))))
