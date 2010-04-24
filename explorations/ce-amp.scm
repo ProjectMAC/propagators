40ace705b6d16bd4665d5f3b6aecd53f9a0e6f08
Gerald Jay Sussman <gjs@mit.edu>
2010-04-24 03:19:10 +0000

    If I specify a resistance for the resistor it works!

--- explorations/ce-amp.scm
+++ explorations/ce-amp.scm
@@ -323,6 +323,7 @@
 	      (V (voltage-source)))
     (let-cells ((n1 (node (the t1 V) (the t1 R)))
 		(n2 (node (the t2 V) (the t2 R))))
+      ((constant 3) (the resistance R))
       ((constant 6) (the strength V))
       ((constant 0) (the potential n2))
       (e:inspectable-object R V n1 n2))))
@@ -367,3 +368,20 @@
   (p:make-terminal-from-current current terminal))
 
 (define ce:current (functionalize c:current))
+
+#|
+ (initialize-scheduler)
+ ;Value: 0
+
+ (define-cell test (resistor-circuit))
+ ;Value: test
+
+ (define-cell answer (the current R test))
+ ;Value: answer
+
+ (run)
+ ;Value: done
+
+ (content answer)
+ ;Value: 2
+|#
