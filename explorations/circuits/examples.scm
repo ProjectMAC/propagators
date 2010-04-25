1017222424139deef1255c1c148837c4e55f4f0b
Gerald Jay Sussman <gjs@mit.edu>
2010-04-25 20:43:27 +0000

    First cut at a slice.  It will error out if it is used wrong.

--- explorations/circuits/examples.scm
+++ explorations/circuits/examples.scm
@@ -43,3 +43,56 @@
  (content answer)
  ;Value: 2
 |#
+
+
+(define (voltage-divider-slice R1 R2)
+  ;; 1. Need to make this contingent on zeroness of output current
+  ;; or maybe approximately correct based on residual current.
+  ;; 2. Also need to verify that (the t2 R1) and (the t1 R2) have 
+  ;; a node in common.
+  (let-cells ((Requiv (resistor))
+	      (ok? (e:= (e:+ (e:terminal-current (the t2 R1))
+			     (e:terminal-current (the t1 R2)))
+			0)))
+    ((constant #t) ok?)
+    (c:+ (the resistance R1)
+	 (the resistance R2)
+	 (the resistance Requiv))
+    (conditional-wire ok? (the t1 R1) (the t1 Requiv))
+    (conditional-wire ok? (the t2 R2) (the t2 Requiv))))
+
+(define (voltage-divider-circuit)
+  (let-cells ((R1 (resistor))
+	      (R2 (resistor))
+	      (V (voltage-source)))
+    (let-cells ((n1 (node (the t1 V) (the t1 R1)))
+		(n2 (node (the t2 R1) (the t1 R2)))
+		(n3 (node (the t2 V) (the t2 R2))))
+      ((constant 2) (the resistance R1))
+      ((constant 4) (the resistance R2))
+      ((constant 6) (the strength V))
+      ((constant 0) (the potential n3))
+      (e:inspectable-object R1 R2 V n1 n2 n3))))
+
+#|
+ (initialize-scheduler)
+ ;Value: 0
+
+ (define-cell test (voltage-divider-circuit))
+ ;Value: test
+
+ (define-cell answer (the potential n2 test))
+ ;Value: answer
+
+ (run)
+ ;Value: done
+
+ (content answer)
+ ;Value: #(*the-nothing*)
+
+ (voltage-divider-slice (the R1 test) (the R2 test))
+ (run)
+ (content answer)
+ 
+|#
+
