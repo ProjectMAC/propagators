56a0e96b763cef022f98ea13ea2e702ffa167633
Gerald Jay Sussman <gjs@mit.edu>
2010-04-25 19:41:22 +0000

    Free Software notices.

--- explorations/circuits/examples.scm
+++ explorations/circuits/examples.scm
@@ -1,3 +1,22 @@
+;;; ----------------------------------------------------------------------
+;;; Copyright 2010 Alexey Radul and Gerald Jay Sussman
+;;; ----------------------------------------------------------------------
+;;; This file is part of Propagator Network Prototype.
+;;; 
+;;; Propagator Network Prototype is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;; 
+;;; Propagator Network Prototype is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;; 
+;;; You should have received a copy of the GNU General Public License
+;;; along with Propagator Network Prototype.  If not, see <http://www.gnu.org/licenses/>.
+;;; ----------------------------------------------------------------------
+
 (define (resistor-circuit)
   (let-cells ((R (resistor))
 	      (V (voltage-source)))
