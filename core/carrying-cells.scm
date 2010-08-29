d3ff1a47ebea0ae169302f079443af3b880e54e9
Gerald Jay Sussman <gjs@mit.edu>
2010-08-29 22:22:49 +0000

    Macrology.  Bawden would be proud^H^H^H^H^Hhorrified. Abstracting out the pile of propagator definitions that are needed to produce the p:cons concept, so that general record structures can be dealt with nicely.

--- core/carrying-cells.scm
+++ core/carrying-cells.scm
@@ -26,7 +26,7 @@
 
 ;;; CONS looks like this:
 #;
- (define (p:carry-cons a-cell d-cell output)
+ (define (p:cons a-cell d-cell output)
    ((constant (cons a-cell d-cell)) output))
 
 ;;; The general version for arbitrary constructors:
@@ -38,22 +38,12 @@
 	   (inputs (map ensure-cell (except-last-pair cells))))
        (execute-propagator     ; To enable the early-access-hack below
 	((constant (apply f inputs)) output))))))
-(define-cell p:carry-cons (function->cell-carrier-constructor cons))
-(define-cell e:carry-cons (expression-style-variant p:carry-cons))
 
-;;; Type tester:
-
-(define carry-pair? pair?)
-(propagatify carry-pair?)
-(define carry-null? null?)
-(propagatify carry-null?)
-
-;;; Accessors are remarkably easy:
-
-(define-propagator (p:carry-car pair-cell output)
-  (p:carry-cons output nothing pair-cell))
-(define-propagator (p:carry-cdr pair-cell output)
-  (p:carry-cons nothing output pair-cell))
+;;; Type testers like pair? are just normal propagators.
+;;; Accessors are just constructors in reverse, like this:
+#;
+ (define-propagator (p:car pair-cell output)
+   (p:cons output nothing pair-cell))
 
 ;;; Expression-style accessors offer an opportunity for a performance
 ;;; hack: if the cell holding the accessed item is already present in
@@ -79,25 +69,94 @@
 	 (accessor (content structure-cell))
 	 (fallback structure-cell)))))
 
-(define-cell e:carry-car (early-access-hack pair? car e:carry-car))
-(define-cell e:carry-cdr (early-access-hack pair? cdr e:carry-cdr))
+;;; To actually define those propagators, you would write
+#|
+ (define-cell p:cons (function->cell-carrier-constructor cons))
+ (define-cell e:cons (expression-style-variant p:cons))
+ (propagatify pair?)
+ (define-propagator (p:car pair-cell output)
+   (p:cons output nothing pair-cell))
+ (define-propagator (p:cdr pair-cell output)
+   (p:cons nothing output pair-cell))
+ (define-cell e:car (early-access-hack pair? car e:car))
+ (define-cell e:cdr (early-access-hack pair? cdr e:cdr))
+|#
 
-;;; Carrying data is standard
+;;; That's what the define-propagator-structure macro is for.
 
-(define p:cons  p:carry-cons)
-(define e:cons  e:carry-cons)
-(define p:pair? p:carry-pair?)
-(define e:pair? e:carry-pair?)
-(define p:car   p:carry-car)
-(define e:car   e:carry-car)
-(define p:cdr   p:carry-cdr)
-(define e:cdr   e:carry-cdr)
+(define-syntax define-structure-propagators
+  (rsc-macro-transformer
+   (lambda (form defn-env)
+     (let* ((type-name (cadr form))
+	    (constructor-name (caddr form))
+	    (defined-constructor-names
+	      (propagator-naming-convention constructor-name))
+	    (accessor-names (cdddr form))
+	    (accessor-count (length accessor-names)))
+       (define (attach-% name)
+	 (symbol '% name))
+       (define (accessor-definition hidden-name name index)
+	 (define (output-reference)
+	   (let ((answer (make-vector accessor-count 'nothing)))
+	     (vector-set! answer index 'output)
+	     (vector->list answer)))
+	 `(define-propagator (,hidden-name structure-cell output)
+	    (,(car defined-constructor-names)
+	     ,@(output-reference)
+	     structure-cell)))
+       (define (early-access-hack-definition hidden-name name)
+	 (let ((expression-variant (cadr (propagator-naming-convention name))))
+	   `(define-cell ,expression-variant
+	      (early-access-hack
+	       ,type-name ,name ,(cadr (propagator-naming-convention hidden-name))))))
+       (define (name-fix-definition hidden-name name)
+	 `(define-cell ,(car (propagator-naming-convention name))
+	    ,(car (propagator-naming-convention hidden-name))))
+       `(begin
+	  (propagatify ,type-name)
+	  (define-by-diagram-variant ,defined-constructor-names
+	    (function->cell-carrier-constructor ,constructor-name))
+	  ,@(map accessor-definition
+		 (map attach-% accessor-names)
+		 accessor-names
+		 (iota accessor-count))
+	  ,@(map name-fix-definition
+		 (map attach-% accessor-names)
+		 accessor-names)
+	  ,@(map early-access-hack-definition
+		 (map attach-% accessor-names)
+		 accessor-names))))))
+
+(define-structure-propagators pair? cons car cdr)
+
+;;; Here are the old names of these until I sweep them out of the code
+(define p:carry-cons  p:cons)
+(define e:carry-cons  e:cons)
+(define p:carry-pair? p:pair?)
+(define e:carry-pair? e:pair?)
+(define p:carry-car   p:car)
+(define e:carry-car   e:car)
+(define p:carry-cdr   p:cdr)
+(define e:carry-cdr   e:cdr)
+
+;;; To make lists out of conses, we need empty lists too.
+(define carry-null? null?)
+(propagatify carry-null?)
 (define p:null? p:carry-null?)
 (define e:null? e:carry-null?)
 
+;;; These guys are really the primitive container devices, from which
+;;; everything else can be made.
 (define-cell p:deposit (function->cell-carrier-constructor identity))
 (define-cell e:deposit (expression-style-variant p:deposit))
 (define-propagator (p:examine place cell)
   (p:deposit cell place))
 (define-cell e:examine
   (early-access-hack cell? identity e:examine))
+
+(define-syntax define-propagator-structure
+  (syntax-rules ()
+    ((define-propagator-structure arg ...)
+     (begin
+       (define-structure-propagators arg ...)
+       (slotful-information-type arg ...)))))
